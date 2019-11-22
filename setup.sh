604110c7f79169147587d2af0e9c5ee5c8079ee8
George Williams <george.williams@gmail.com>
2019-11-22 02:02:47 +0200

    adding finalization actions to setup

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ fi
 
 gnome-terminal --disable-factory --load-config=./termboarddmesg.cfg -- bash -c "./termboarddmesg.sh $1; bash" &
 PID=$!
-#sleep 0.5
+sleep 0.5
 CMD="pgrep -P $PID"
 C1=$(eval $CMD)
 CMD="pgrep -P $C1"
@@ -28,15 +28,52 @@ C2=$(eval $CMD)
 CMD="ps --no-headers -p $C2 -o pid,tty | awk '{ print \$2 }'"
 TTY=$(eval $CMD)
 DEV="/dev/$TTY"
-#echo "" > $DEV
-#echo "# When you are ready, hit ENTER" > $DEV
-#echo "./termboard.sh" > $DEV
-#echo "$PID,$C1,$C2,$TTY,$DEV"
+BOARD=$C2
+echo "BOARD INFO: $PID,$C1,$C2,$TTY,$DEV,$BOARD"
 
 gnome-terminal --disable-factory --load-config=./termpcdmesg.cfg -- bash -c "./termpcdmesg.sh $1; bash" &
+PID=$!
+sleep 0.5
+CMD="pgrep -P $PID"
+C1=$(eval $CMD)
+CMD="pgrep -P $C1"
+C2=$(eval $CMD)
+CMD="ps --no-headers -p $C2 -o pid,tty | awk '{ print \$2 }'"
+TTY=$(eval $CMD)
+DEV="/dev/$TTY"
+PC=$C2
+echo "PC INFO: $PID,$C1,$C2,$TTY,$DEV,$PC"
 
 gnome-terminal --disable-factory --load-config=./termminicom.cfg -- bash -c "./termminicom.sh $1; bash" &
+PID=$!
+sleep 0.5
+CMD="pgrep -P $PID"
+C1=$(eval $CMD)
+CMD="pgrep -P $C1"
+C2=$(eval $CMD)
+CMD="ps --no-headers -p $C2 -o pid,tty | awk '{ print \$2 }'"
+TTY=$(eval $CMD)
+DEV="/dev/$TTY"
+MINICOM=$C2
+echo "MINICOM INFO: $PID,$C1,$C2,$TTY,$DEV,$MINICOM"
 
-gnome-terminal --disable-factory --load-config=./termsearch.cfg -- bash -c "./termsearch.sh $1; bash" &
+gnome-terminal --disable-factory --load-config=./termsearch.cfg -- bash -c "./termsearch.sh $1; bash" 
 
+# Now cleanup everything
+
+echo "Killing board terminal..."
+kill $BOARD
+echo "Killing pc terminal..."
+kill $PC
+echo "Killing minicom terminal..."
+kill $MINICOM
+
+echo "Logging into board and making sure logger is shutdown..."
+sudo sshpass -p "root" sudo sshpass -p "root" ssh -o StrictHostKeyChecking=no root@192.168.32.4 "killall boarddmesg.sh"
+
+echo "Syncing board log files..."
+sudo sshpass -p "root" scp -r root@192.168.32.4:/home/root/logs/* ./logs/
+
+
+echo "Done."
 
